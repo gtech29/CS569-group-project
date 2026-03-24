e48d4751bbdc2c02d1e1d7ef7a92141f1a65549b
gtech29 <juan.rodriguez.517@my.csun.edu>
2026-03-23 21:03:14 -0700

    feat: implement 80/10/10 data splitting for model training
    Created prepare_training_data.py to automate dataset preparation.
    
    Implemented a two-stage split using train_test_split to achieve an 80% training, 10% validation, and 10% test distribution.
    
    Configured the script to load from steam_reviews_sample_10k.csv and save the resulting splits to train.csv, val.csv, and test.csv.
    
    Established a random_state=42 to ensure reproducible results.

--- src/prepare_training_data.py
+++ src/prepare_training_data.py
@@ -2,14 +2,18 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 
-# 1. Setup the path to find your sample data
+# 1. Setup the path to find data
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SAMPLE_PATH = os.path.join(ROOT, "data", "processed", "steam_reviews_sample_10k.csv")
-
+OUT_DIR = os.path.join(ROOT, "data", "processed")
+TRAIN_PATH = os.path.join(OUT_DIR, "train.csv")
+VAL_PATH = os.path.join(OUT_DIR, "val.csv")
+TEST_PATH = os.path.join(OUT_DIR, "test.csv")
 
 def main():
     print("Loading the 10k sample dataset...")
     df = pd.read_csv(SAMPLE_PATH)
+    
 
     # 2. First Split: 80% for Training, 20% Temporary Leftovers
     # test_size=0.2 means 20% goes to the temp pile, leaving 80% for training
@@ -20,13 +24,22 @@ def main():
     # test_size=0.5 means split the temp pile 50/50
     val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
 
-    # 4. Verify our math!
-    print("--- Data Split Complete ---")
-    print(f"Total Original Rows: {len(df)}")
-    print(f"Training Set (80%): {len(train_df)} rows")
-    print(f"Validation Set (10%): {len(val_df)} rows")
-    print(f"Test Set (10%): {len(test_df)} rows")
+    # 4. Verify 
+    # print("--- Data Split Complete ---")
+    # print(f"Total Original Rows: {len(df)}")
+    # print(f"Training Set (80%): {len(train_df)} rows")
+    # print(f"Validation Set (10%): {len(val_df)} rows")
+    # print(f"Test Set (10%): {len(test_df)} rows")
+    
+    train_df.to_csv(TRAIN_PATH, index=False)
+    val_df.to_csv(VAL_PATH, index=False)
+    test_df.to_csv(TEST_PATH, index=False)
 
-
+    # Stopping here for now might, need to sort the df by timestamp due to chronological splits, 
+    # but will do more research on this
+    
+    
+    
+    
 if __name__ == "__main__":
     main()
